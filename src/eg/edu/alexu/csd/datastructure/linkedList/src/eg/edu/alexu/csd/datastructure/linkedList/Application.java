da3efdb56b5a6570680b9867748b90556cdf6edf
seif7666 <58919329+seif7666@users.noreply.github.com>
2020-03-15 22:41:22 +0200

    Add files via upload
    The newest

---
+++ src/eg/edu/alexu/csd/datastructure/linkedList/src/eg/edu/alexu/csd/datastructure/linkedList/Application.java
@@ -0,0 +1,190 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Application {
+    private static Scanner in=new Scanner(System.in);
+    private static  Polynomials polynomial=new Polynomials();
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t WELCOME");
+        menu();
+
+        int choice;
+        boolean exit=false;
+        while(!exit){
+            choice=getInt();
+            switch(choice){
+                case 0:exit=true;break;
+                case 1:setPolynomial();break;
+                case 2:printPolynomial();break;
+                case 3:addPolynomials();break;
+                case 4:subtractPolynomials();break;
+                case 5:multiplyPolynomial();break;
+                case 6:evaluatePolynomial();break;
+                case 7:clear();break;
+                case 8:menu();break;
+                default:
+                    System.out.println("Error enter valid choice!");
+            }
+        }
+        System.out.println("Press any key to continue.");
+        in.nextLine();
+
+
+
+    }
+    public static void setPolynomial() {
+        char poly;
+        System.out.println("Insert the variable name: A, B or C");
+        poly = getChar();
+        poly = polynomial.checkPoly(poly);
+        System.out.println("Please enter the number of terms of the polynomial.");
+        int terms = getInt();
+        if (terms < 0 || terms > 1000)
+            throw new ArithmeticException("Invalid number of terms");
+        int[][] input = new int[terms][2];
+        for (int i = 0; i < terms; i++) {
+            System.out.print("Enter the coefficient of " + (i + 1) + "th term:");
+            input[i][0] = getInt();
+            System.out.print("Enter the exponent " + (i + 1) + "th term:");
+            input[i][1] = getInt();
+        }
+        polynomial.setPolynomial(poly,input);
+        System.out.println("Polynomial" + poly + " is successfully set.");
+        System.out.println(polynomial.print(poly));
+    }
+    public static void printPolynomial(){
+        char poly;
+        System.out.println("Insert the variable name: A, B or C");
+        poly=getChar();
+        poly=polynomial.checkPoly(poly);
+        System.out.println(polynomial.print(poly));
+    }
+    public static void addPolynomials(){
+        char poly1,poly2;
+        System.out.println("Insert the variable name of the first polynomial: A, B or C");
+        poly1=getChar();
+        poly1=polynomial.checkPoly(poly1);
+        System.out.println("Insert the variable name of the second polynomial: A, B or C");
+        poly2=getChar();
+        poly2=polynomial.checkPoly(poly2);
+        int[][]R=polynomial.add(poly1,poly2);
+        printPolynomial(R);
+    }
+    public static  void subtractPolynomials(){
+        char poly1,poly2;
+        System.out.println("Insert the variable name of the first polynomial: A, B or C");
+        poly1=getChar();
+        poly1=polynomial.checkPoly(poly1);
+        System.out.println("Insert the variable name of the second polynomial: A, B or C");
+        poly2=getChar();
+        poly2=polynomial.checkPoly(poly2);
+        int[][]R=polynomial.subtract(poly1,poly2);
+        printPolynomial(R);
+    }
+    public static  void multiplyPolynomial(){
+        char poly1,poly2;
+        System.out.println("Insert the variable name of the first polynomial: A, B or C");
+        poly1=getChar();
+        poly1=polynomial.checkPoly(poly1);
+        System.out.println("Insert the variable name of the second polynomial: A, B or C");
+        poly2=getChar();
+        poly2=polynomial.checkPoly(poly2);
+        int[][]R=polynomial.multiply(poly1,poly2);
+        printPolynomial(R);
+    }
+    public static void evaluatePolynomial(){
+        char poly;
+        System.out.println("Insert the variable name of the polynomial: A, B or C");
+        poly=getChar();
+        poly=polynomial.checkPoly(poly);
+        float value;
+        System.out.println("What is the value to be evaluated?");
+        value=getFloat();
+        float ans=polynomial.evaluatePolynomial(poly,value);
+        System.out.println("The answer is :"+ans);
+    }
+    public static void clear(){
+        System.out.println("Insert the variable name of the polynomial: A, B or C");
+        char poly=getChar();
+        poly=polynomial.checkPoly(poly);
+        polynomial.clearPolynomial(poly);
+    }
+
+    public static void printPolynomial(int[][] R){
+        System.out.print("R=");
+        for (int i=0 ;i<R.length;i++){
+            if(R[i][0]==0&&R[i][1]==0)
+                break;
+            System.out.print("( "+R[i][0]+" , "+R[i][1]+" )");
+        }
+
+    }
+    public static char getChar(){
+        char choice;
+        while(true){
+            System.out.print("Enter a letter: \r");
+            choice=in.next().charAt(0);
+            if((choice>='a'&&choice<='z')||(choice>='A'&&choice<='Z'))
+                return choice;
+            else
+                System.out.println("Error\nEnter a valid Letter!");
+        }
+    }
+
+    public static void menu(){
+        System.out.println("Please choose an action.");
+        System.out.println("\n-----------------------\n");
+        System.out.println("1- Set a polynomial variable\n"+
+        "2- Print the value of a polynomial variable\n"+
+        "3- Add two polynomials\n"+
+        "4- Subtract two polynomials\n"+
+        "5- Multiply two polynomials\n"+
+        "6- Evaluate a polynomial at some point\n"+
+        "7- Clear a polynomial variable\n"+
+                "8-To show menu\n"+
+                "0-To Exit"
+        );
+
+    }
+    public static int getInt(){
+        while(true){
+            System.out.print("Enter your choice: \r");
+            int choice;
+            try{
+                choice=in.nextInt();
+                in.nextLine();
+                return choice;
+            }
+            catch(InputMismatchException e){
+                in.nextLine();
+                System.out.println("Enter valid integer!");
+            }
+        }
+
+    }
+    public static float getFloat(){
+        while(true){
+            System.out.print("Enter your choice: \r");
+            float choice;
+            try{
+                choice=in.nextFloat();
+                in.nextLine();
+                return choice;
+            }
+            catch(InputMismatchException e){
+                in.nextLine();
+                System.out.println("Enter valid integer!");
+            }
+        }
+
+    }
+
+
+
+
+
+}
---
+++ src/eg/edu/alexu/csd/datastructure/linkedList/src/eg/edu/alexu/csd/datastructure/linkedList/Polynomials.java
@@ -0,0 +1,323 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+
+import java.awt.event.MouseAdapter;
+import java.util.Arrays;
+
+public class Polynomials implements IPolynomialSolver {
+
+    SingleLinked A = new SingleLinked();
+    SingleLinked B = new SingleLinked();
+    SingleLinked C = new SingleLinked();
+    SingleLinked R = new SingleLinked();
+
+
+    @Override
+    public void setPolynomial(char poly, int[][] terms) {
+        SingleLinked temp = new SingleLinked();
+        poly=checkPoly(poly);
+        int n = terms.length;
+        for(int i =0; i<n;i++)
+            temp.add(terms[i]);
+        temp= (SingleLinked) sortList(temp);
+        switch (poly){
+            case 'A':A=temp;break;
+            case 'B':B=temp;break;
+            case 'C':C=temp;break;
+        }
+    }
+
+
+    @Override
+    public String print(char poly) {
+        SingleLinked temp = new SingleLinked();
+        poly = checkPoly(poly);
+        int[] temp1;
+        int i=0;
+        //The list  is sorted at set.
+
+        switch (poly){
+            case 'A':temp=A;break;
+            case 'B':temp=B;break;
+            case 'C':temp=C;break;
+        }
+
+        String s ="";
+        if(temp.isEmpty())
+            return null;
+        while (i<temp.size()){
+            temp1 = (int[]) temp.get(i);
+            if (i==0) {
+                if (temp1[0] == 1 || temp1[0] == 0){}
+                else if (temp1[0] != 0 && temp1[0] != 1)
+                    s += temp1[0];
+                if (temp1[1] == 0){}
+                else if (temp1[1] != 0)
+                    s += "x^" + temp1[1];
+            }
+            else {
+                if (temp1[0]<0){}
+                else
+                    s+="+";
+                if (temp1[0] == 1 || temp1[0] == 0){
+                }
+                else if (temp1[0] != 0 && temp1[0] != 1)
+                    s += temp1[0];
+                if (temp1[1] == 0){}
+                else if (temp1[1] != 0)
+                    s += "x^" + temp1[1];
+            }
+            i++;
+        }
+
+        return s;
+    }
+
+    @Override
+    public void clearPolynomial(char poly) {
+        poly=checkPoly(poly);
+        if(poly == 'A')
+            A.clear();
+        else if (poly=='B')
+            B.clear();
+        else
+            C.clear();
+    }
+
+    @Override
+    public float evaluatePolynomial(char poly, float value) {
+        SingleLinked temp = new SingleLinked();
+        poly=checkPoly(poly);
+        switch (poly){
+            case 'A':temp=A;
+            case 'B':temp=B;
+            case 'C':temp=C;
+        }
+        float ans =0;
+        int i=0;
+        int[] temp1 ;
+        while (temp.hasNext(i)){
+            temp1 = (int[]) temp.get(i);
+            ans+=temp1[0]* Math.pow(value,temp1[1]);
+
+        }
+        return ans;
+    }
+
+    @Override
+    public int[][] add(char poly1, char poly2) {
+      SingleLinked polynomial1=null;
+      SingleLinked polynomial2=null;
+       switch (poly1){
+           case 'A':polynomial1=A;break;
+            case 'B':polynomial1=B;break;
+            case 'C':polynomial1=C;break;
+        }
+       switch (poly2){
+            case 'A':polynomial2=A;break;
+            case 'B':polynomial2=B;break;
+            case 'C':polynomial2=C;break;
+        }
+        if(polynomial1==null) {
+            System.out.println(poly1 + " is not set.");
+            return null;
+        }
+        if(polynomial2==null) {
+            System.out.println(poly2 + " is not set.");
+            return null;
+        }
+        if(polynomial1.size()==0&&polynomial2.size()==0) {
+            System.out.println("We are here");
+            return new int[][]{};
+        }
+
+       int[][] R=new int[polynomial1.size()+polynomial2.size()][2];
+        int counter=0,i=0,j=0;
+        while(i<polynomial1.size()||j<polynomial2.size()){
+            int[] temp1;
+            int[] temp2;
+            if(j==polynomial2.size()) {
+                temp1=(int[])polynomial1.get(i);
+                R[counter] = temp1;
+                i++;
+            }
+            else if(i==polynomial1.size()) {
+                temp2=(int[])polynomial2.get(j);
+                R[counter] = temp2;
+                j++;
+            }
+            else {
+                temp1 = (int[]) polynomial1.get(i);
+                temp2 = (int[]) polynomial2.get(j);
+
+             if (temp1[1] > temp2[1]) {
+//               R[counter][0]=polynomial1[i][0];
+//               R[counter][1]=polynomial1[i][1];
+                    R[counter] = temp1;
+                    i++;
+                } else if (temp1[1] < temp2[1]) {
+                    R[counter] = temp2;
+                    j++;
+                } else {//Equal powers
+                    R[counter][0] = temp1[0] + temp2[0];
+                    R[counter][1] = temp2[1];
+                    i++;
+                    j++;
+                }
+            }
+            counter++;
+        }
+        return R;
+    }
+    @Override
+    public int[][] subtract(char poly1, char poly2) {
+        poly1=checkPoly(poly1);
+        poly2=checkPoly(poly2);
+        SingleLinked polynomial1=null;
+        SingleLinked polynomial2=null;
+        switch (poly1){
+            case 'A':polynomial1=A;break;
+            case 'B':polynomial1=B;break;
+            case 'C':polynomial1=C;break;
+        }
+        switch (poly2){
+            case 'A':polynomial2=A;break;
+            case 'B':polynomial2=B;break;
+            case 'C':polynomial2=C;break;
+        }
+        if(polynomial1==null) {
+            System.out.println(poly1 + " is not set.");
+            return null;
+        }
+        if(polynomial2==null) {
+            System.out.println(poly2 + " is not set.");
+            return null;
+        }
+        if(polynomial1.size()==0&&polynomial2.size()==0)
+            return new int[][]{};
+
+        int[][] R=new int[polynomial1.size()+polynomial2.size()][2];
+        int counter=0,i=0,j=0;
+        while(i<polynomial1.size()||j<polynomial2.size()){
+            int[] temp1;
+            int[] temp2;
+            if(j==polynomial2.size()) {
+                temp1=(int[])polynomial1.get(i);
+                R[counter] = temp1;
+                i++;
+            }
+            else if(i==polynomial1.size()) {
+                temp2=(int[])polynomial2.get(j);
+                R[counter][0] = -temp2[0];
+                R[counter][1] = temp2[1];
+                j++;
+            }
+            else {
+                temp1 = (int[]) polynomial1.get(i);
+                temp2 = (int[]) polynomial2.get(j);
+
+                if (temp1[1] > temp2[1]) {
+//               R[counter][0]=polynomial1[i][0];
+//               R[counter][1]=polynomial1[i][1];
+                    R[counter] = temp1;
+                    i++;
+                } else if (temp1[1] < temp2[1]) {
+                    R[counter][0] = -temp2[0];
+                    R[counter][1] = temp2[1];
+                    j++;
+                } else {//Equal powers
+                    R[counter][0] = temp1[0] - temp2[0];
+                    R[counter][1] = temp2[1];
+                    i++;
+                    j++;
+                }
+            }
+            counter++;
+        }
+        return R;
+    }
+
+    @Override
+    public int[][] multiply(char poly1, char poly2) {
+        poly1 = checkPoly(poly1);
+        poly2 = checkPoly(poly2);
+        SingleLinked polynomial1 = null;
+        SingleLinked polynomial2 = null;
+        switch (poly1) {
+            case 'A':
+                polynomial1 = A;break;
+            case 'B':
+                polynomial1 = B;break;
+            case 'C':
+                polynomial1 = C;break;
+        }
+        switch (poly2) {
+            case 'A':
+                polynomial2 = A;break;
+            case 'B':
+                polynomial2 = B;break;
+            case 'C':
+                polynomial2 = C;break;
+        }
+        if (polynomial1 == null) {
+            System.out.println(poly1 + " is not set.");
+            return null;
+        }
+        if (polynomial2 == null) {
+            System.out.println(poly2 + " is not set.");
+            return null;
+        }
+        if (polynomial1.size() == 0)
+            return new int[][]{};
+        else if (polynomial2.size() == 0)
+            return new int[][]{};
+        int[][] R = new int[polynomial1.size() * polynomial2.size()][2];
+        int counter = 0;
+        for (int i = 0; i < polynomial1.size(); i++) {
+            for (int j = 0; j < polynomial2.size(); j++) {
+                int[] temp1 = (int[]) polynomial1.get(i);
+                int[] temp2 = (int[]) polynomial2.get(j);
+                R[counter][0] = temp1[0] * temp2[0];
+                R[counter][1] = temp1[1] + temp2[1];
+                counter++;
+            }
+        }
+        //The result must be sorted.
+        SingleLinked x=A;
+        setPolynomial('A',R);
+        R=new int[A.size()][2];
+        //A is sorted .
+        for(int i=0;i<A.size();i++){
+            R[i]=(int[])A.get(i);
+        }
+        A=x;
+        return R;
+    }
+
+    public char checkPoly(char poly){
+        if(poly=='a'||poly=='A')
+            return 'A';
+        if(poly=='b'||poly=='B')
+            return 'B';
+        if(poly=='c'||poly=='C')
+            return 'C';
+        if(poly=='r'||poly=='R')
+            return 'R';
+        throw new RuntimeException();
+
+    }
+    public ILinkedList sortList(ILinkedList x){
+        System.out.println("size is "+x.size());
+        for(int i=0;i<x.size();i++) {
+            for (int j = 0; j < (x.size()- 1); j++) {
+                int[] temp1 = (int[]) x.get(j);
+                int[] temp2 = (int[]) x.get(j + 1);
+                if (temp1[1] < temp2[1]) {
+                    x.set(j, temp2);
+                    x.set(j + 1, temp1);
+                }
+
+            }
+        }
+        return x;
+    }
+}
