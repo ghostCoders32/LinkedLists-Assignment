6ae4a499de3bc9715abfbf3643e3a6bf689f8e44
Ziad-H-Mahgoub <61160877+Ziad-H-Mahgoub@users.noreply.github.com>
2020-03-20 17:45:03 +0200

    Update ILinkedList.java

--- src/seif/com/ILinkedList.java
+++ src/seif/com/ILinkedList.java
@@ -1,46 +1,57 @@
-package seif.com;
-
+package eg.edu.alexu.csd.datastructure.linkedList;
 public interface ILinkedList {
     /**
      * Inserts a specified element at the specified position in the list.
+     *
      * @param index
      * @param element
      */
     public void add(int index, Object element);
+
     /**
      * Inserts the specified element at the end of the list.
+     *
      * @param element
      */
     public void add(Object element);
+
     /**
      * @param index
      * @return the element at the specified position in this list.
      */
-    public Object get(int index) ;
+    public Object get(int index);
+
     /**
      * Replaces the element at the specified position in this list with the
      * specified element.
+     *
      * @param index
      * @param element
      */
     public void set(int index, Object element);
+
     /**
      * Removes all of the elements from this list.
      */
     public void clear();
+
     /**
      * @return true if this list contains no elements.
      */
     public boolean isEmpty();
+
     /**
      * Removes the element at the specified position in this list.
+     *
      * @param index
      */
     public void remove(int index);
+
     /**
      * @return the number of elements in this list.
      */
     public int size();
+
     /**
      * @param fromIndex
      * @param toIndex
@@ -48,6 +59,7 @@ public interface ILinkedList {
      * fromIndex and toIndex, inclusively.
      */
     public ILinkedList sublist(int fromIndex, int toIndex);
+
     /**
      * @param o
      * @return true if this list contains an element with the same value as the
